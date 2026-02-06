27f1364e0a2b506b136b625a51330cceac7f2d76
Mihir Laud <mihirlaud@gmail.com>
2026-02-06 17:59:47 -0600

    Add high score and custom font

--- ui/sidebar/sidebar.gd
+++ ui/sidebar/sidebar.gd
@@ -1,30 +1,36 @@
 extends Control
 
+var hi_score
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	anchor_top = 0
-	anchor_left = 0
-	anchor_right = 1.0 / 3.0
-	anchor_bottom = 1.0
+	var screen_size = get_viewport_rect().size
+	hi_score = 0
 	
-	$ScoreLabel.text = "Score: 0"
-	$MultLabel.text = "Mult: 1.0"
-	$SpeedLabel.text = "Speed: 50"
+	$ScoreLabel.text = "SCORE:\n0"
+	$ScoreLabel.size = Vector2(screen_size.x / 3, 80)
+	
+	$HiScoreLabel.text = "HI SCORE:\n0"
+	$HiScoreLabel.size = Vector2(screen_size.x / 3, 80)
+	$HiScoreLabel.position.y = 90
+	
+	$ColorRect.size = Vector2(screen_size.x / 3, screen_size.y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_game_mult_changed(new_value: Variant) -> void:
-	$MultLabel.text = str("Mult: ", new_value)
+	pass
 
 func _on_game_score_changed(new_value: Variant) -> void:
-	$ScoreLabel.text = str("Score: ", int(round(new_value)))
+	$ScoreLabel.text = str("SCORE:\n", int(round(new_value)))
+	if new_value > hi_score:
+		$HiScoreLabel.text = str("HI SCORE:\n", int(round(new_value)))
+		hi_score = new_value
 
 func reset() -> void:
-	$ScoreLabel.text = "Score: 0"
-	$MultLabel.text = "Mult: 1.0"
-	$SpeedLabel.text = "Speed: 50"
+	$ScoreLabel.text = "SCORE:\n0"
 
 func _on_game_speed_changed(new_value: Variant) -> void:
-	$SpeedLabel.text = str("Speed: ", int(new_value / 10.0))
+	pass
